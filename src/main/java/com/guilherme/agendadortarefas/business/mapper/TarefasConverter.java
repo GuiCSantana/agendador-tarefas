b43d88cf64f96ca859d06415492d2ecbf7adc722
Guilherme <guilhermecsantana0@gmail.com>
2025-12-10 13:45:04 -0300

    feat: substituir TarefasDTO por record e adicionar tratamento global de exceções
    - Removido TarefasDTO e criado TarefasDTORecord
    - Ajustados converters, service, controller e repository
    - Adicionado GlobalExceptionHandler e UnauthorizedException
    - Atualizado application.properties

--- src/main/java/com/guilherme/agendadortarefas/business/mapper/TarefasConverter.java
+++ src/main/java/com/guilherme/agendadortarefas/business/mapper/TarefasConverter.java
@@ -1,6 +1,6 @@
 package com.guilherme.agendadortarefas.business.mapper;
 
-import com.guilherme.agendadortarefas.business.dto.TarefasDTO;
+import com.guilherme.agendadortarefas.business.dto.TarefasDTORecord;
 import com.guilherme.agendadortarefas.infrastructure.entity.TarefasEntity;
 import org.mapstruct.Mapper;
 
@@ -9,11 +9,11 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface TarefasConverter {
 
-    TarefasEntity paraTarefaEntity(TarefasDTO dto);
+    TarefasEntity paraTarefaEntity(TarefasDTORecord dto);
 
-    TarefasDTO paraTarefaDTO(TarefasEntity entity);
+    TarefasDTORecord paraTarefaDTO(TarefasEntity entity);
 
-    List<TarefasEntity> paraListaTarefasEntity(List<TarefasDTO> dtos);
+    List<TarefasEntity> paraListaTarefasEntity(List<TarefasDTORecord> dtos);
 
-    List<TarefasDTO> paraListaTarefasDTO(List<TarefasEntity> entities);
+    List<TarefasDTORecord> paraListaTarefasDTORecord(List<TarefasEntity> entities);
 }
